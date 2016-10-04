8b1de640d637b62330ca50b1b4f7de79991b8d51
Ashton210z <ashton210z@gmail.com>
2016-10-04 10:53:28 -0700

    Public metatags in Unity

---
+++ Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DontDestroy : MonoBehaviour {
+
+    public InputField input;
+    public string playerName;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void loadNextScene()
+    {
+        playerName = input.text;
+        SceneManager.LoadScene("Main");
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,7 +13,6 @@ public class PlayerController : MonoBehaviour {
     public float speed;
     public float tilt;
     public Boundary boundary;
-
     public GameObject shot;
     public Transform shotSpawn; //shotSpawn.transform.position
     public float fireRate;
